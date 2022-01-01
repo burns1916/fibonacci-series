dc61a66a6d9758f1054c764939c94cedcd2c366c
Ryan Burns <ryankburns@gmail.com>
2022-01-01 12:34:46 -0800

    added looping solution

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -5,6 +5,19 @@ function fibonacci(num) {
   } else {
     return fibonacci(num - 1) + fibonacci(num - 2)
   }
+  /* fibonacci looping:
+
+    fibonacci(num) {
+      let a = 0, b = 1, f = 1;
+      for (var i = 2; i <= num; i++) {
+        f = a + b;
+        a = b;
+        b = f;
+      }
+      return f
+    }
+
+  */
 }
 
 console.log(fibonacci())
@@ -30,3 +43,7 @@ module.exports = fibonacci;
 // Please add your pseudocode to this file
   //
 // And a written explanation of your solution
+  //recursion: takes in the number and breaks it down in the recursion forumla until reduced to 
+  //1 then added together.
+  //looping: starts at index 3 calculates a, b, and f until i <= the given number
+
